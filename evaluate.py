7a5bc046c224b31d65dc01bf53e8d14e792418ce
DanialDaeHyunNam <danialnam89@gmail.com>
2020-05-26 00:35:02 +0900

    Modified evaluate according to pep8

--- evaluate.py
+++ evaluate.py
@@ -22,40 +22,40 @@ class ArenaEvaluator:
 
     def _eval(self, gt_fname, rec_fname):
         gt_playlists = load_json(gt_fname)
-        gt_dict = {g["id"]: g for g in gt_playlists}
+        gt_dict = {g['id']: g for g in gt_playlists}
         rec_playlists = load_json(rec_fname)
 
-        gt_ids = set([g["id"] for g in gt_playlists])
-        rec_ids = set([r["id"] for r in rec_playlists])
+        gt_ids = set([g['id'] for g in gt_playlists])
+        rec_ids = set([r['id'] for r in rec_playlists])
 
         if gt_ids != rec_ids:
-            raise Exception("결과의 플레이리스트 수가 올바르지 않습니다.")
+            raise Exception('결과의 플레이리스트 수가 올바르지 않습니다.')
 
-        rec_song_counts = [len(p["songs"]) for p in rec_playlists]
-        rec_tag_counts = [len(p["tags"]) for p in rec_playlists]
+        rec_song_counts = [len(p['songs']) for p in rec_playlists]
+        rec_tag_counts = [len(p['tags']) for p in rec_playlists]
 
         if set(rec_song_counts) != set([100]):
-            raise Exception("추천 곡 결과의 개수가 맞지 않습니다.")
+            raise Exception('추천 곡 결과의 개수가 맞지 않습니다.')
 
         if set(rec_tag_counts) != set([10]):
-            raise Exception("추천 태그 결과의 개수가 맞지 않습니다.")
+            raise Exception('추천 태그 결과의 개수가 맞지 않습니다.')
 
-        rec_unique_song_counts = [len(set(p["songs"])) for p in rec_playlists]
-        rec_unique_tag_counts = [len(set(p["tags"])) for p in rec_playlists]
+        rec_unique_song_counts = [len(set(p['songs'])) for p in rec_playlists]
+        rec_unique_tag_counts = [len(set(p['tags'])) for p in rec_playlists]
 
         if set(rec_unique_song_counts) != set([100]):
-            raise Exception("한 플레이리스트에 중복된 곡 추천은 허용되지 않습니다.")
+            raise Exception('한 플레이리스트에 중복된 곡 추천은 허용되지 않습니다.')
 
         if set(rec_unique_tag_counts) != set([10]):
-            raise Exception("한 플레이리스트에 중복된 태그 추천은 허용되지 않습니다.")
+            raise Exception('한 플레이리스트에 중복된 태그 추천은 허용되지 않습니다.')
 
         music_ndcg = 0.0
         tag_ndcg = 0.0
 
         for rec in rec_playlists:
-            gt = gt_dict[rec["id"]]
-            music_ndcg += self._ndcg(gt["songs"], rec["songs"][:100])
-            tag_ndcg += self._ndcg(gt["tags"], rec["tags"][:10])
+            gt = gt_dict[rec['id']]
+            music_ndcg += self._ndcg(gt['songs'], rec['songs'][:100])
+            tag_ndcg += self._ndcg(gt['tags'], rec['tags'][:10])
 
         music_ndcg = music_ndcg / len(rec_playlists)
         tag_ndcg = tag_ndcg / len(rec_playlists)
@@ -66,12 +66,12 @@ class ArenaEvaluator:
     def evaluate(self, gt_fname, rec_fname):
         try:
             music_ndcg, tag_ndcg, score = self._eval(gt_fname, rec_fname)
-            print(f"Music nDCG: {music_ndcg:.6}")
-            print(f"Tag nDCG: {tag_ndcg:.6}")
-            print(f"Score: {score:.6}")
+            print(f'Music nDCG: {music_ndcg:.6}')
+            print(f'Tag nDCG: {tag_ndcg:.6}')
+            print(f'Score: {score:.6}')
         except Exception as e:
             print(e)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     fire.Fire(ArenaEvaluator)
